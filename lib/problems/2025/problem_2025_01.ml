8f3c8d822b246d71360199f06357bf2466b77687
Nikita Gaidakov <ngaidakov@podfather.com>
2025-12-02 21:03:46 +0000

    Use module functor for day 1!

--- lib/problems/2025/problem_2025_01.ml
+++ lib/problems/2025/problem_2025_01.ml
@@ -5,18 +5,6 @@ type turns = int list
 
 open Import
 
-let (%) x y =
-  let r = x mod y in
-  if r < 0 then r + y else r
-
-let solve_with (count_zeroes : state:int -> turn:int -> int) (turns : turns) : int =
-  List.fold_left (fun (state, zeroes) turn ->
-    let state' = (state + turn) % 100 in
-    let added_zeroes = count_zeroes ~state ~turn in
-    ( state', zeroes + added_zeroes )
-  ) (50, 0) turns
-  |> snd
-
 module Parse : sig
   val parse : string -> (turns, string) result
 end = struct
@@ -40,23 +28,32 @@ end = struct
   let print (turns : turns) : string = String.concat ", " @@ List.map string_of_int turns
 end
 
-module Part_1 : sig
+let (%) x y =
+  let r = x mod y in
+  if r < 0 then r + y else r
+
+module Solution(Part : sig
+  val count_zeroes : state:int -> turn:int -> int
+end) : sig
   val run : string -> (string, string) result
 end = struct
-  let run (input : string) : (string, string) result =
-    Parse.parse input
-    |> Result.map (solve_with (fun ~state ~turn ->
-      if (state + turn) % 100 = 0 then 1 else 0
-    )
-    >> string_of_int)
+  let solve : turns -> int =
+    List.fold_left (fun (state, zeroes) turn ->
+      let state' = (state + turn) % 100 in
+      let added_zeroes = Part.count_zeroes ~state ~turn in
+      ( state', zeroes + added_zeroes )
+    ) (50, 0)
+    >> snd
+
+  let run = Parse.parse >> Result.map (solve >> string_of_int)
 end
 
-module Part_2 = struct
-  let run (input : string) : (string, string) result =
-    Parse.parse input
-    |> Result.map (solve_with (fun ~state ~turn ->
-      let sum = state + turn in
-      abs (sum / 100) + if sum <= 0 && state <> 0 then 1 else 0
-    )
-    >> string_of_int)
-end
+module Part_1 = Solution(struct
+  let count_zeroes ~state ~turn = if (state + turn) % 100 = 0 then 1 else 0
+end)
+
+module Part_2 = Solution(struct
+  let count_zeroes ~state ~turn =
+    let sum = state + turn in
+    abs (sum / 100) + if sum <= 0 && state <> 0 then 1 else 0
+end)
