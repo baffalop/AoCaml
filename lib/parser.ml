63a3abce032b575ac5084d54491f284bedca2137
Nikita Gaidakov <ngaidakov@podfather.com>
2025-12-04 20:26:59 +0000

    Parse day 4 (I think?)

--- lib/parser.ml
+++ lib/parser.ml
@@ -3,11 +3,3 @@ open Angstrom
 let u_dec : int t =
   let* digits = take_while1 (function '0'..'9' -> true | _ -> false) in
   return @@ int_of_string digits
-
-module GridMap = Map.Make(struct
-  type t = int * int
-  let compare = compare
-end)
-
-let grid_of (p : 'a option t) : 'a GridMap.t t =
-  failwith "todo"
--- lib/problems/2025/problem_2025_04.ml
+++ lib/problems/2025/problem_2025_04.ml
@@ -2,36 +2,32 @@ let year = 2025
 let day = 4
 
 open Import
+open Grid
 
-module GridMap = Parser.GridMap
+type map = [`R] GridMap.t
 
-type roll = R
+let parse : string -> map =
+  grid_of @@ function
+    | '@' -> Some `R
+    | _ -> None
 
-type map = roll GridMap.t
-
-module Parse : sig
-  val parse : string -> (map, string) result
-end = struct
-  open Angstrom
-
-  let map : (map t) = failwith "todo"
-
-  let parse : string -> (map, string) result =
-    parse_string ~consume:Prefix map
-end
+let show (map : map) : string =
+  GridMap.fold (fun (x, y) _ res -> Printf.sprintf "%s\n%d, %d" res x y)
+    map "\n"
 
 module Solution(Part : sig
   val solve : map -> int
 end) : sig
   val run : string -> (string, string) result
 end = struct
-  let run = Parse.parse >> Result.map (Part.solve >> string_of_int)
+  let run = parse >> show (* Part.solve >> string_of_int *)
+    >> Result.ok
 end
 
 module Part_1 = Solution(struct
-  let solve = failwith "todo"
+  let solve _ = failwith "todo"
 end)
 
 module Part_2 = Solution(struct
-  let solve = failwith "todo"
+  let solve _ = failwith "todo"
 end)
