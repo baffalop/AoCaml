e8d7cc75712499f0b749989e45282131eeda105a
Nikita Gaidakov <ngaidakov@podfather.com>
2025-12-05 07:56:29 +0000

    Parse day 5

--- lib/parser.ml
+++ lib/parser.ml
@@ -3,3 +3,6 @@ open Angstrom
 let u_dec : int t =
   let* digits = take_while1 (function '0'..'9' -> true | _ -> false) in
   return @@ int_of_string digits
+
+let lines_of (p : 'a t) : 'a list t =
+  sep_by1 (char '\n') p
---
+++ lib/problems/2025/problem_2025_05.ml
@@ -0,0 +1,57 @@
+let year = 2025
+let day = 5
+
+open Import
+
+type range = int * int
+
+module Inventory = struct
+  type t = {
+    ranges: range list;
+    ingredients: int list;
+  }
+
+  let show { ranges; ingredients } : string =
+    Fmt.(str "Ranges:\n%a\n\nIngredients:\n%a\n"
+      (list ~sep:(any "\n") @@ pair ~sep:(any ", ") int int) ranges
+      (list ~sep:(any "\n") int) ingredients
+    )
+end
+
+module Parse : sig
+  val parse : string -> (Inventory.t, string) result
+end = struct
+  open Angstrom
+
+  let range : range t =
+    let* a = Parser.u_dec in
+    char '-' *>
+    let* b = Parser.u_dec in
+    return (a, b)
+
+  let inventory : Inventory.t t =
+    let* ranges = Parser.lines_of range in
+    string "\n\n" *>
+    let* ingredients = Parser.lines_of Parser.u_dec in
+    let open Inventory in
+    return { ranges; ingredients }
+
+  let parse : string -> (Inventory.t, string) result =
+    parse_string ~consume:Prefix inventory
+end
+
+module Solution(Part : sig
+  val solve : Inventory.t -> int
+end) : sig
+  val run : string -> (string, string) result
+end = struct
+  let run = Parse.parse >> Result.map Inventory.show (*Part.solve >> string_of_int*)
+end
+
+module Part_1 = Solution(struct
+  let solve _ = failwith "todo"
+end)
+
+module Part_2 = Solution(struct
+  let solve _ = failwith "todo"
+end)
