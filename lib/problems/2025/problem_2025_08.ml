ce93d2f30e7a438d86056c04e3378c09ef29ec80
Nikita Gaidakov <ngaidakov@podfather.com>
2025-12-08 07:28:43 +0000

    Parse day 8 part 1

---
+++ lib/problems/2025/problem_2025_08.ml
@@ -0,0 +1,34 @@
+let year = 2025
+let day = 8
+
+open Import
+
+type coord = int * int * int
+type coords = coord list
+
+let parse : string -> (coords, string) result =
+  let open Angstrom in
+  let open Parser in
+  parse_string ~consume:Prefix @@ lines_of @@
+  let triple x y z = (x, y, z) in
+  triple <$> u_dec <* char ',' <*> u_dec <* char ',' <*> u_dec
+
+let show : coords -> string =
+  List.map (fun (x, y, z) -> Printf.sprintf "(%d, %d, %d)" x y z)
+  >> String.concat "\n"
+
+module Solution(Part : sig
+  val solve : coords -> int
+end) : sig
+  val run : string -> (string, string) result
+end = struct
+  let run = parse >> Result.map show (*Part.solve >> string_of_int*)
+end
+
+module Part_1 = Solution(struct
+  let solve _ = failwith "part 1"
+end)
+
+module Part_2 = Solution(struct
+  let solve _ = failwith "part 2"
+end)
