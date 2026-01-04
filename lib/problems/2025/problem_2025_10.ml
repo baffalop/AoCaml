f63b20155e04c76bfc6346001df8f99111f075f9
Nikita Gaidakov <ngaidakov@podfather.com>
2026-01-04 11:34:26 +0000

    Use Base: day 10

--- lib/problems/2025/problem_2025_10.ml
+++ lib/problems/2025/problem_2025_10.ml
@@ -1,6 +1,7 @@
 let year = 2025
 let day = 10
 
+open Base
 open Import
 
 module IntSet = (val Set.make_showable (module Int) (Fmt.int))
@@ -12,7 +13,7 @@ type machine = {
 } [@@deriving show]
 
 module Parse : sig
-  val parse : string -> (machine list, string) result
+  val parse : string -> (machine list, string) Result.t
 end = struct
   open Angstrom
   open Parsers
@@ -23,8 +24,8 @@ end = struct
       char '.' *> return false;
     ] in
     let lights = lights_bool
-      |> List.mapi (fun i x -> if x then Some i else None)
-      |> List.filter_map id
+      |> List.mapi ~f:(fun i x -> if x then Some i else None)
+      |> List.filter_map ~f:id
       |> IntSet.of_list
     in
     char ' ' *>
@@ -35,56 +36,56 @@ end = struct
     let* joltages = bounded_by '{' '}' @@ sep_by1 (char ',') u_dec in
     return { lights; buttons; joltages }
 
-  let parse : string -> (machine list, string) result =
+  let parse : string -> (machine list, string) Result.t =
     parse_string ~consume:Prefix @@ lines_of machine
 end
 
-let show_set = IntSet.elements >> List.map string_of_int >> String.concat ","
-let show_buttons = List.map show_set >> String.concat " "
+let show_set = IntSet.elements >> List.map ~f:Int.to_string >> String.concat ~sep:","
+let show_buttons = List.map ~f:show_set >> String.concat ~sep:" "
 let show : machine list -> string =
-  List.map (fun { lights; buttons; joltages } ->
+  List.map ~f:(fun { lights; buttons; joltages } ->
     Printf.sprintf "Lights: %s\nButtons: %s\nJoltages: %s\n"
       (show_set lights)
       (show_buttons buttons)
-      (joltages |> List.map string_of_int |> String.concat ",")
+      (joltages |> List.map ~f:Int.to_string |> String.concat ~sep:",")
   )
-  >> String.concat "\n"
+  >> String.concat ~sep:"\n"
 
 module Solution(Part : sig
   val solve : machine list -> int
 end) = struct
-  let run = Parse.parse >> Result.map (Part.solve >> string_of_int)
+  let run = Parse.parse >> Result.map ~f:(Part.solve >> Int.to_string)
 end
 
 let rec choose (n : int) (pool : 'a list) : 'a list list =
   if n <= 0 then [[]]
   else pool
-  |> List.take (List.length pool - n + 1)
-  |> List.mapi (fun i x ->
-    choose (n - 1) (List.drop (i + 1) pool)
-    |> List.map (List.cons x)
+  |> flip List.take (List.length pool - n + 1)
+  |> List.mapi ~f:(fun i x ->
+    choose (n - 1) (List.drop pool (i + 1))
+    |> List.map ~f:(List.cons x)
   )
-  |> List.flatten
+  |> List.join
 
 module Part_1 = Solution(struct
   let press_button (lights : IntSet.t) (button : IntSet.t) =
     IntSet.union lights button
     |> IntSet.filter (fun light ->
-      IntSet.mem light button <> IntSet.mem light lights
+      Bool.(IntSet.mem light button <> IntSet.mem light lights)
     )
 
   let min_presses { lights; buttons; _ } =
     let rec try_choose (n : int) =
       let chosen = choose n buttons in
-      let target_reached = chosen |> List.exists (fun buttons ->
-          List.fold_left press_button IntSet.empty buttons
+      let target_reached = chosen |> List.exists ~f:(fun buttons ->
+          List.fold ~f:press_button ~init:IntSet.empty buttons
           |> IntSet.equal lights
       ) in
       if target_reached then n else try_choose (n + 1)
     in
     try_choose 1
 
-  let solve : machine list -> int = List.map min_presses >> List.fold_left (+) 0
+  let solve : machine list -> int = List.map ~f:min_presses >> List.fold ~init:0 ~f:(+)
 end)
 
 module Part_2 = Solution(struct
