85c63c7c7abe8b602e4299764cfdb10eb1a11eec
Nikita Gaidakov <ngaidakov@podfather.com>
2026-01-04 09:05:16 +0000

    Use Base: day 3

--- lib/problems/2025/problem_2025_03.ml
+++ lib/problems/2025/problem_2025_03.ml
@@ -1,41 +1,43 @@
 let year = 2025
 let day = 3
 
+open Base
 open Import
 
 type jbank = int list [@@deriving show]
 
 let parse : string -> jbank list =
-  String.split_on_char '\n'
-  >> List.map (String.to_seq >> List.of_seq
-  >> List.map (Char.escaped >> int_of_string))
+  String.split ~on:'\n'
+  >> List.map ~f:(String.to_list
+  >> List.map ~f:(Char.escaped >> Int.of_string))
 
 module Solution(Part : sig
   val max_joltage : jbank -> int
 end) : sig
-  val run : string -> (string, string) result
+  val run : string -> (string, string) Result.t
 end = struct
   let run =
     parse
-    >> List.map Part.max_joltage
-    >> List.fold_left (+) 0
-    >> string_of_int
-    (* >> List.map string_of_int >> String.concat ", " *)
-    >> Result.ok
+    >> List.map ~f:Part.max_joltage
+    >> List.fold ~init:0 ~f:(+)
+    >> Int.to_string
+    (* >> List.map ~f:string_of_int >> String.concat ~sep:", " *)
+    >> Result.return
 end
 
 let max_digit : jbank -> (int * int) =
-  List.fold_left (fun (i, (res_i, res)) d ->
-    (i + 1, if d > res then (i, d) else (res_i, res))
-  ) (0, (0, 0))
+  List.fold ~init:(0, (0, 0))
+    ~f:(fun (i, (res_i, res)) d ->
+      (i + 1, if d > res then (i, d) else (res_i, res))
+    )
   >> snd
 
 let max_joltage_of (n : int) : jbank -> int =
   let rec max_joltage (jolts : int) (n : int) (bank : jbank) =
     if n = 0 then jolts else
     let next_n = n - 1 in
-    let (i, joltage) = max_digit @@ List.take (max 1 @@ List.length bank - next_n) bank in
-    max_joltage (jolts * 10 + joltage) next_n @@ List.drop (i + 1) bank
+    let (i, joltage) = max_digit @@ List.take bank (max 1 @@ List.length bank - next_n) in
+    max_joltage (jolts * 10 + joltage) next_n @@ List.drop bank (i + 1)
   in
   max_joltage 0 n
 
