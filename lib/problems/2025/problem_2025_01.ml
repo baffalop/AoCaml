a342293136a663f0054a12a8ca5a4d940862b2d8
Nikita Gaidakov <ngaidakov@podfather.com>
2026-01-04 08:48:06 +0000

    Start adding Base: day 1

--- lib/problems/2025/problem_2025_01.ml
+++ lib/problems/2025/problem_2025_01.ml
@@ -1,12 +1,13 @@
 let year = 2025
 let day = 1
 
-type turns = int list
-
+open Base
 open Import
 
+type turns = int list
+
 module Parse : sig
-  val parse : string -> (turns, string) result
+  val parse : string -> (turns, string) Result.t
 end = struct
   open Angstrom
   open Parsers
@@ -19,28 +20,24 @@ end = struct
     let* n = u_dec in
     return (dir * n)
 
-  let parse : string -> (turns, string) result =
+  let parse : string -> (turns, string) Result.t =
     parse_string ~consume:Prefix @@ lines_of turn
 end
-
-let (%) x y =
-  let r = x mod y in
-  if r < 0 then r + y else r
 
 module Solution(Part : sig
   val count_zeroes : state:int -> turn:int -> int
 end) : sig
-  val run : string -> (string, string) result
+  val run : string -> (string, string) Result.t
 end = struct
   let solve : turns -> int =
-    List.fold_left (fun (state, zeroes) turn ->
+    List.fold_left ~init:(50, 0) ~f:(fun (state, zeroes) turn ->
       let state' = (state + turn) % 100 in
       let added_zeroes = Part.count_zeroes ~state ~turn in
       ( state', zeroes + added_zeroes )
-    ) (50, 0)
+    )
     >> snd
 
-  let run = Parse.parse >> Result.map (solve >> string_of_int)
+  let run = Parse.parse >> Result.map ~f:(solve >> Int.to_string)
 end
 
 module Part_1 = Solution(struct
